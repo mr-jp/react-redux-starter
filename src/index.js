a69a1aa3c9b7924fb1c82673b2246755323368af
Jason <jasonphoon@gmail.com>
2018-03-05 19:51:04 +0800

    Manage courses page

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import {Provider} from 'react-redux'; //attaches store to our React container co
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import {loadCourses} from './actions/courseActions';
+import {loadAuthors} from './actions/authorActions';
 import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
 store.dispatch(loadCourses());  //dispatch load courses on page load
+store.dispatch(loadAuthors());
 
 render(
   <Provider store={store}>
