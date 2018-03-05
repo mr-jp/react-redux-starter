16d86c9257c4c8bfd55d7697949c1a75356c236e
Jason <jasonphoon@gmail.com>
2018-03-05 18:31:25 +0800

    Course list as rows and separate presentation components

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux'; //attaches store to our React container components
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import {loadCourses} from './actions/courseActions';
 import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());  //dispatch load courses on page load
 
 render(
   <Provider store={store}>
