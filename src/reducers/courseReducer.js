ac6191ef803bdfdecc9f3b268c7e5bc890ccfbd2
Jason <jasonphoon@gmail.com>
2018-03-05 14:16:53 +0800

    Basic Redux Flow

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,5 +1,5 @@
 export default function courseReducer(state = [], action) {
-  switch(action) {
+  switch(action.type) {
     case 'CREATE_COURSE':
       //return new state (new array with extra value)
       return [...state, Object.assign({}, action.course)];
